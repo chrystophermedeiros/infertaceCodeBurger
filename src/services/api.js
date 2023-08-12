bd3b41b6e5ead8023a2cb1d685a8daa15a89b7a9
chrystophermedeiros <chrystopher312@gmail.com>
2023-08-12 10:19:01 -0300

    baseURl da api trocada para nuvem

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3003'
+  baseURL: 'https://apicodeburger-production.up.railway.app/'
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
